aa9e45662dfa94a95df9ace3ad13c07084159cf6
Felipe Bello Dultra <felipe_bello_dultra@hotmail.com>
2024-10-24 17:04:24 -0300

    feat: add favicon, shadcn/ui and some default metadatas

---
+++ src/app/home/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+
+export default function HomePage() {
+  return (
+    <div>
+      <Button onClick={console.log}>Button</Button>
+    </div>
+  );
+}
