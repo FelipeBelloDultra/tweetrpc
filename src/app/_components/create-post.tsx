dd35a715ca92e1db5328eabbc8657c2058c628b0
Felipe Bello Dultra <felipe_bello_dultra@hotmail.com>
2024-10-29 19:20:42 -0300

    feat(posts): implement way to create posts with tRPC

--- src/app/_components/create-post.tsx
+++ src/app/_components/create-post.tsx
@@ -1,19 +1,41 @@
 "use client";
 
-import { ClerkLoaded, ClerkLoading, UserButton } from "@clerk/nextjs";
-import { SendIcon } from "lucide-react";
+import { ClerkLoaded, ClerkLoading, UserButton, useAuth } from "@clerk/nextjs";
+import { LoaderCircleIcon, SendIcon } from "lucide-react";
 import { Button, Skeleton, Textarea } from "./ui";
 import { useState } from "react";
+import { api } from "@/trpc/react";
 
 const MAX_TEXTAREA_LENGTH = 255;
 
 export default function CreatePost() {
   const [post, setPost] = useState("");
+  const utils = api.useUtils();
+  const { userId, signOut } = useAuth();
+  const createPost = api.post.create.useMutation({
+    onSuccess: async () => {
+      await utils.post.invalidate();
+      setPost("");
+    },
+  });
 
-  function handleChangePost(post: string) {
+  function onChangePost(post: string) {
     if (post.length > MAX_TEXTAREA_LENGTH) return;
 
     setPost(post);
+  }
+
+  async function handleSubmitPost() {
+    if (!userId) {
+      await signOut();
+
+      return;
+    }
+
+    createPost.mutate({
+      authorId: userId,
+      content: post,
+    });
   }
 
   return (
@@ -39,20 +61,28 @@ export default function CreatePost() {
         </div>
 
         <Textarea
+          disabled={createPost.isPending}
           className="border-grey-700 h-32 max-h-32 resize-none border py-4 pl-24 pr-4 font-medium"
           maxLength={MAX_TEXTAREA_LENGTH}
           autoFocus
           placeholder="What would you like to share?"
           value={post}
-          onChange={(e) => handleChangePost(e.target.value)}
+          onChange={(e) => onChangePost(e.target.value)}
         />
       </div>
 
       <Button
-        className="ml-auto mt-4 flex items-center gap-2 font-bold"
-        onClick={() => console.log(post)}
+        className="ml-auto mt-4 flex w-full max-w-24 items-center gap-2 font-bold"
+        onClick={handleSubmitPost}
+        disabled={createPost.isPending}
       >
-        Share <SendIcon />
+        {createPost.isPending ? (
+          <LoaderCircleIcon className="animate-spin" />
+        ) : (
+          <>
+            Share <SendIcon />
+          </>
+        )}
       </Button>
     </>
   );
