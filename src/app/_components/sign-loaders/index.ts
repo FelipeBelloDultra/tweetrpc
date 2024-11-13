bfd1938a8742e877ff988cffc18b28934889ec4f
Felipe Bello Dultra <felipe_bello_dultra@hotmail.com>
2024-11-13 18:50:49 -0300

    fix(components): remove unused component

--- src/app/_components/sign-loaders/index.ts
+++
@@ -1,9 +0,0 @@
-import { SignSkeletonHeader } from "./sign-skeleton-header";
-import { SignSkeletonMain } from "./sign-skeleton-main";
-import { SignSkeletonRoot } from "./sign-skeleton-root";
-
-export const SignLoaders = {
-  Root: SignSkeletonRoot,
-  Header: SignSkeletonHeader,
-  Main: SignSkeletonMain,
-};
