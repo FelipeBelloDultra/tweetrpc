0fa42de725f490bd560b63118219d143ad2f21fc
Felipe Bello Dultra <felipe_bello_dultra@hotmail.com>
2024-11-16 17:16:15 -0300

    feat(posts): finish post list and add loader

--- src/app/_components/post-list.tsx
+++ src/app/_components/post-list.tsx
@@ -1,9 +1,22 @@
-import { api } from "@/trpc/server";
+"use client";
+
+import { api } from "@/trpc/react";
 import { EmptyList } from "./empty-list";
 import { Post } from "./post";
+import { Skeleton } from "./ui";
 
-export async function PostList() {
-  const data = await api.post.fetchAll();
+export function PostList() {
+  const { data, isPending } = api.post.fetchAll.useQuery();
+
+  if (isPending || !data) {
+    return (
+      <div className="mt-6 flex flex-col gap-4">
+        {Array.from({ length: 6 }).map((_, i) => (
+          <PostLoader key={`${i}-post-loader`} />
+        ))}
+      </div>
+    );
+  }
 
   return (
     <div className="pb-10">
@@ -22,6 +35,28 @@ export async function PostList() {
       ) : (
         <EmptyList />
       )}
+    </div>
+  );
+}
+
+function PostLoader() {
+  return (
+    <div className="flex items-start gap-2 rounded-md bg-white p-4">
+      <Skeleton className="size-12 rounded-full" />
+
+      <div className="flex h-11 flex-1 flex-col">
+        <div className="mb-1 flex items-center justify-between">
+          <Skeleton className="inline-block h-5 w-32" />
+
+          <Skeleton className="inline-block h-5 w-24" />
+        </div>
+
+        <div className="mt-auto flex h-2 gap-1">
+          <Skeleton className="inline-block h-full w-8" />
+          <Skeleton className="inline-block h-full w-4" />
+          <Skeleton className="inline-block h-full w-12" />
+        </div>
+      </div>
     </div>
   );
 }
