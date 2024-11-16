e9cc950a93028fadb7357d0bcca896e7720643b2
Felipe Bello Dultra <felipe_bello_dultra@hotmail.com>
2024-11-16 12:24:43 -0300

    feat: create tRPC procedure to fetchAll posts - and mapping with clerk api

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -1,6 +1,9 @@
 import { z } from "zod";
 
 import { createTRPCRouter, protectedProcedure } from "@/server/api/trpc";
+import { type Post } from "@prisma/client";
+import { clerkClient } from "@clerk/nextjs/server";
+import { TRPCError } from "@trpc/server";
 
 export const postRouter = createTRPCRouter({
   create: protectedProcedure
@@ -13,4 +16,51 @@ export const postRouter = createTRPCRouter({
         },
       });
     }),
+
+  fetchAll: protectedProcedure.query(async ({ ctx }) => {
+    const posts = await ctx.db.post.findMany({
+      take: 100,
+      orderBy: [{ createdAt: "desc" }],
+    });
+
+    return await addUserDataToPost(posts);
+  }),
 });
+
+async function addUserDataToPost(posts: Array<Post>) {
+  const clerkClientInstance = await clerkClient();
+  const authorsId = posts.map((post) => post.authorId);
+  const accounts = await clerkClientInstance.users.getUserList({
+    userId: authorsId,
+    limit: 100,
+  });
+
+  return posts.map((post) => {
+    const author = accounts.data.find(
+      (account) => account.id === post.authorId,
+    );
+
+    if (!author) {
+      console.error("AUTHOR NOT FOUND", post);
+      throw new TRPCError({
+        code: "INTERNAL_SERVER_ERROR",
+        message: `Author for post not found. POST ID: ${post.id}, AUTHOR ID: ${post.authorId}`,
+      });
+    }
+
+    if (!author.firstName || !author.lastName) {
+      throw new TRPCError({
+        code: "INTERNAL_SERVER_ERROR",
+        message: `Author has first name or last name. AUTHOR ID: ${author.id}`,
+      });
+    }
+
+    return {
+      post,
+      author: {
+        ...author,
+        name: `${author.firstName} ${author.lastName}`,
+      },
+    };
+  });
+}
